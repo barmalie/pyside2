9c89b3932985971de5d90548ece937596754c772
russos <ro0ne@mail.ru>
2022-12-08 19:30:32 +0300

    commit

--- DevPyQt-pyside_6/scripts/Практика 3/a_Повторение/spin.py
+++ DevPyQt-pyside_6/scripts/Практика 3/a_Повторение/spin.py
@@ -19,48 +19,55 @@ class Window(QtWidgets.QWidget):
         self.initTimer(1000)
 
 
-def unitUi(self):
-    self.b1 = QtWidgets.QPushButton("push")
-    self.b2 = QtWidgets.QPushButton("clean")
-    self.spin = QtWidgets.QSpinBox()
-    self.text = QtWidgets.QLineEdit()
-    self.text_1 = QtWidgets.QPlainTextEdit()
-    hor_layout = QtWidgets.QHBoxLayout
-    hor_layout.addWidget(self.text)
-    hor_layout.addWidget(self.b1)
-    hor_layout.addWidget(self.b2)
-    main_layout = QtWidgets.QVBoxLayout()
-    main_layout.addLayout(hor_layout)
-    main_layout.addLayout(self.spin)
-    main_layout.addWidget(self.text_1)
+    def unitUi(self):
+        #self.b1 = QtWidgets.QPushButton("push")
+        self.b2 = QtWidgets.QPushButton("clean")
+        self.spin = QtWidgets.QSpinBox()
+        self.text = QtWidgets.QLineEdit()
+        self.text_1 = QtWidgets.QPlainTextEdit()
+        hor_layout = QtWidgets.QHBoxLayout
+        hor_layout.addWidget(self.text)
+        #hor_layout.addWidget(self.b1)
+        hor_layout.addWidget(self.b2)
+        main_layout = QtWidgets.QVBoxLayout()
+        main_layout.addLayout(hor_layout)
+        main_layout.addLayout(self.spin)
+        main_layout.addWidget(self.text_1)
 
-    self.setLayout(main_layout)
-
-
-def initTimers(self, param):
-    self.timer = QtCore.QTimer()
-    self.timer.setInterval(param)
-    self.timer.start()
+        self.setLayout(main_layout)
 
 
-def initSignals(self):
-    self.b2.clicked.connect(self.text_p.clear)
-    self.b1.clicked.connect(self.startGen)
-
-    self.spin.clicked.connect(self.set.Timer)
-    self.timer.timeout.connect(self.setTextEdit)
-
-def setTimer(self):
-    data_spin = (int(self.spin.text())
-    self.timer.setInterval((data_spin+1)*100)
+    def initTimers(self, param):
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(param)
+        self.timer.start()
 
 
-def startGen(self):
-    self.gen = (val for i in self.text.text())
+    def initSignals(self):
+        self.b2.clicked.connect(self.text_p.clear)
+        #self.b1.clicked.connect(self.startGen)
+
+        self.spin.textChanged.connect(self.setTimer)
+        self.timer.timeout.connect(self.setTextEdit)
+
+    def setTimer(self):
+        data_spin = (int(self.spin.text())
+        self.timer.setInterval((data_spin+1)*100)
 
 
-def setTextEdit(self):
-    try:
-        self.text_1.appendPlainText(next(self.gen))
-    except(StopIteration):
-        pass
+    def startGen(self):
+        self.gen = (val for val in self.text.text())
+
+
+    def setTextEdit(self):
+        data = self.text.text()
+        if len(data) >1:
+        self.text_1.appendPlainText(data[0])
+        self.text.setText(data[1:])
+    elif len(daat) ==1:
+        self.text_1.appendPlainText(data[0])
+        self.text.clear()
+        # try:
+        #     self.text_1.appendPlainText(next(self.gen))
+        # except(StopIteration):
+        #     self.text_1.appendPlainText("except")
