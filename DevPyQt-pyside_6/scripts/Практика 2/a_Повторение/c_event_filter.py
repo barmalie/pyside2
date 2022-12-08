98d68b6dc2c6a287fd5e36a9e58c8962a0a2e33d
russos <ro0ne@mail.ru>
2022-12-08 18:22:44 +0300

    commit

--- DevPyQt-pyside_6/scripts/Практика 2/a_Повторение/c_event_filter.py
+++ DevPyQt-pyside_6/scripts/Практика 2/a_Повторение/c_event_filter.py
@@ -1,24 +1,53 @@
-"""
-Файл для повторения темы фильтр событий
+import sys
 
-Напомнить про работу с фильтром событий.
+from PySide6 import QtWidgets, QtCore
 
-Предлагается создать кликабельный QLabel с текстом "Красивая кнопка",
-используя html - теги, покрасить разные части текста на нём в разные цвета
-(красивая - красным, кнопка - синим)
-"""
-
-from PySide6 import QtWidgets
-from a_signals import Ui_form
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.initUi()
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication()
+    def initUi(self) -> None:
+        """
+        Доинициализация Ui
+
+        :return: None
+        """
+
+        self.setFixedSize(300, 100)
+
+        self.label = QtWidgets.QLabel("<H1>Нажми</H1> на меня")
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label.installEventFilter(self)  # Установка фильтра событий на конкретный виджет
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+    def eventFilter(self, watched: QtCore.QObject, event: QtCore.QEvent) -> bool:
+        """
+        Настройка дополнительного поведения виджетов
+
+        :param watched: QtCore.QObject
+        :param event: QtCore.QEvent
+        :return: bool
+        """
+
+        if watched == self.label and event.type() == QtCore.QEvent.Type.MouseButtonPress:
+            print("mouse pressed")
+
+        if watched == self.label and event.type() == QtCore.QEvent.Type.Wheel:
+            print("wheel changed")
+            print(event.angleDelta())
+
+        return super(Window, self).eventFilter(watched, event)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
 
     window = Window()
     window.show()
